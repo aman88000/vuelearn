701a8a1cc9614b239e3e04da9e280cac70dca137
aman88000 <aman88000@gmail.com>
2022-05-11 23:03:20 +0530

    v-bind with classes and id

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,12 @@
 <p>{{name}}</p>
 <p v-text="pText"></p>
 <p v-html="pHtml"></p>
+<h3 v-bind:id="headingID">Heading h3</h3>
+<button v-bind:disabled="flag">Disable or enable</button>
+<p v-bind:class="active" class="abc">Active</p>
+<p v-bind:class="isLogin && 'login'">Login class</p>
+<p v-bind:class="isLogin ? 'login': 'logout'">Login/Logout</p>
+<p v-bind:class="[isLogin && 'class1', 'class2']">Array classes</p>
 </template>
 <script>
 export default{
@@ -11,7 +17,12 @@ export default{
         return {
             name: 'Aman',
             pText: 'Testing for text added in p tag',
-            pHtml: '<h3>Hello Testing Added H3 html tag</h3>'
+            pHtml: '<h3>Hello Testing Added H3 html tag</h3>',
+            headingID: 'Header-id',
+            flag: true,
+            active: 'active-class',
+            isLogin: false
+
 
         }
     }
