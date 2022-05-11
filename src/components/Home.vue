49006cc819556256005e0eab23b3e4bd13e8ba04
aman88000 <aman88000@gmail.com>
2022-05-11 22:45:19 +0530

    v-text using

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
 <h1>Home Component in vue </h1>
 <p>{{name}}</p>
+<p v-text="pText"></p>
 </template>
 <script>
 export default{
@@ -8,7 +9,8 @@ export default{
     data(){
         return {
             name: 'Aman',
-            
+            pText: 'Testing for text added in p tag'
+
         }
     }
 }
