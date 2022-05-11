5cb6eff246572c71cfbb9b80d36daf428eafc5b4
aman88000 <aman88000@gmail.com>
2022-05-11 22:42:48 +0530

    added dynamic value from data

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<h1>Home Component in vue </h1>
+<p>{{name}}</p>
+</template>
+<script>
+export default{
+    name: 'Home-vue',
+    data(){
+        return {
+            name: 'Aman',
+            
+        }
+    }
+}
+</script>
+<style></style>
