2eaa293f88464936588b93838e26854a3aed8ae1
aman88000 <aman88000@gmail.com>
2022-05-11 22:46:35 +0530

    v-html using

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,7 @@
 <h1>Home Component in vue </h1>
 <p>{{name}}</p>
 <p v-text="pText"></p>
+<p v-html="pHtml"></p>
 </template>
 <script>
 export default{
@@ -9,7 +10,8 @@ export default{
     data(){
         return {
             name: 'Aman',
-            pText: 'Testing for text added in p tag'
+            pText: 'Testing for text added in p tag',
+            pHtml: '<h3>Hello Testing Added H3 html tag</h3>'
 
         }
     }
