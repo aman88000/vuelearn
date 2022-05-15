e5ee2ce342099b99f86eb23d1498538f17b4c474
aman88000 <aman88000@gmail.com>
2022-05-15 18:27:53 +0530

    watch immediate using in vue js

--- src/components/Watchers.vue
+++ src/components/Watchers.vue
@@ -1,30 +1,34 @@
 <template>
-    <div>
-        <h1>Watcher used in vue js and its properties like immediate and deep</h1>
-        <h2>{{name}}</h2>
-        <button @click="changeName">Change name</button>
-    </div>
+<div>
+    <h1>Watcher used in vue js and its properties like immediate and deep</h1>
+    <h2>{{name}}</h2>
+    <button @click="changeName">Change name</button>
+</div>
 </template>
 
 <script>
-    export default {
-        name: 'WatchersVue',
-        data(){
-            return {
-                name: 'aman'
-            }
-        },
-        methods: {
-            changeName(){
-                this.name = 'Deep'
-            }
-        },
-        watch: {
-            name(currValue, preValue){
-                console.log('called', currValue, preValue)
-            }
+export default {
+    name: 'WatchersVue',
+    data() {
+        return {
+            name: 'aman'
         }
+    },
+    methods: {
+        changeName() {
+            this.name = 'Deep'
+        }
+    },
+    watch: {
+        name: {
+            handler(currValue, preValue) {
+                console.log('called', currValue, preValue)
+            },
+            immediate: true
+        }
+
     }
+}
 </script>
 
 <style lang="scss" scoped>
